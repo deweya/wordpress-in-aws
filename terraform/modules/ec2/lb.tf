977098ca307663ea6a644adf54bc300910a8c55c
Austin Dewey <deweya964@gmail.com>
2020-10-08 10:12:46 -0500

    adding lb healthchecks

--- terraform/modules/ec2/lb.tf
+++ terraform/modules/ec2/lb.tf
@@ -11,6 +11,11 @@ resource "aws_lb_target_group" "this" {
     port = 80
     protocol = "HTTP"
     vpc_id = var.vpc_id
+
+    health_check {
+        path = "/wordpress/wp-content/index.php"
+        matcher = "200-299"
+    }
 }
 
 resource "aws_lb_target_group_attachment" "this" {
